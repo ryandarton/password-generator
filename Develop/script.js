dba392f0a2918bd40ae09d8dd1d08dd01a554684
Ryan Darton <ryan@timemachinevideo.com>
2024-01-23 21:22:31 -0700

    Add psuedocode to help meet acceptance criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,25 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
+
+// When button is clicked,
+//   prompted for the length of the password
+//   -user chooses between 8 - 128 characters
+//   asked for character types to include in the password
+//   - user confirms whether or not to include lowercase, uppercase, numeric, and / or special characters - input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-
+  // Write the password to the #password textarea.
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
