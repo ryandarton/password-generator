32726bb8beaf89ebbf6bea58541bb4ce4ad1b529
Ryan Darton <ryan@timemachinevideo.com>
2024-01-25 20:27:51 -0700

    Make modal window appear when user clicks button.

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,15 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 // Get references to the settings card
-var settingsCard = document.querySelector('.settings-card');
-
-// When the button is clicked, show the settings card
+var settingsCard = document.querySelector('#settings-card');
+// Function for showing the settings card
 function showSettingsCard() {
   settingsCard.style.display = 'block';
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', console.log('button clicked'));
+// Function for hiding the settings card
+function hideSettingsCard() {
+  settingsCard.style.display = 'none';
+}
 
 // When button is clicked,
 //   prompted for the length of the password
@@ -21,12 +21,12 @@ generateBtn.addEventListener('click', console.log('button clicked'));
 // THEN a password is generated that matches the selected criteria
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-  // Write the password to the #password textarea.
-}
+//   passwordText.value = password;
+//   // Write the password to the #password textarea.
+// }
 
 //generateBtn.addEventListener('click', writePassword);
